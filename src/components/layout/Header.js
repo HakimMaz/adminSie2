224d69d5a53340d95774671651ea657afbc5579b
SIE <hakim.brown93@gmail.com>
2020-01-23 15:02:06 +0100

    first commit adminSie

---
+++ src/components/layout/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const myHeader = () => {
+    
+    return (
+        <div>
+            {/* <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+
+                    <Typography variant="h6" noWrap>
+                        Espace administrateur
+          </Typography>
+                </Toolbar>
+            </AppBar> */}
+
+        </div>
+
+    )
+}
+export default myHeader;
